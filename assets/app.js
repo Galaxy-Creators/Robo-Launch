30b177ce9393954958c8dc129102353a9301bd08
Brooke W <bwedeward@gmail.com>
2020-04-12 20:11:58 -0500

    add image result to html page

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 
 $('.stuff').on('click', function (event) {
     event.preventDefault();
-    var queryURL = 'https://api.giphy.com/v1/stickers/search?q=aliens&api_key=yj2GYkgWF6vExu23oEOyov2Ydg9xDUGU&limit=5&rating=g'
+    var queryURL = 'https://api.giphy.com/v1/stickers/search?q=aliens&api_key=yj2GYkgWF6vExu23oEOyov2Ydg9xDUGU&limit=1&rating=g'
     console.log(queryURL);
 
     $.ajax({
@@ -10,6 +10,20 @@ $('.stuff').on('click', function (event) {
       }).then(function(response) {
         var results = response.data;
         console.log(results)
-        console.log('hi')
+       
+        var imageRes = results[0].images.fixed_width_small_still.url
+        console.log(imageRes)
+
+        // var res = $("<img>")
+        // res.attr("src", imageRes)
+        // res.attr("height", "150")
+        // res.attr("width", "150")
+        // $("#steve-image").append(res)
+
+        var steveImage = $("#steve-image")
+        steveImage.css("background-image", "url("+imageRes+")")
+        steveImage.css("background-repeat", "no-repeat")
+        steveImage.css("background-position", "center")
+        steveImage.css("background-size", "contain")
       });
 })
