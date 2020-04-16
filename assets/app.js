30ecd769115e9851b621350f3284c2a270041703
Firecraker <mezamic000@gmail.com>
2020-04-15 20:59:43 -0500

    added a randomizer to pull just one advice strip

--- assets/app.js
+++ assets/app.js
@@ -1,49 +1,50 @@
-$('#submit').on('click', function (event) {
-    event.preventDefault();
-    var random = Math.floor(Math.random() * 30) + 1  
-    var queryURL = 'https://api.giphy.com/v1/stickers/search?q=aliens&api_key=yj2GYkgWF6vExu23oEOyov2Ydg9xDUGU&limit=1&offset='+random+'&rating=g'
-    console.log(queryURL);
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        var results = response.data;
-        console.log(results)
-       
-        var imageRes = results[0].images.original.url
-        console.log(imageRes)
-
-        // var res = $("<img>")
-        // res.attr("src", imageRes)
-        // res.attr("height", "150")
-        // res.attr("width", "150")
-        // $("#steve-image").append(res)
-
-        var steveImage = $("#steve-image")
-        steveImage.css("background-image", "url("+imageRes+")")
-        steveImage.css("background-repeat", "no-repeat")
-        steveImage.css("background-position", "center")
-        steveImage.css("background-size", "contain")
-      });
-})
 $("#submit").on("click", function (event) {
-  event.preventDefault();
-  var topics = $('#topics').val();
-	var adviceQueryURL = "https://api.adviceslip.com/advice/search/" + topics;
-  console.log(topics);
-  console.log(adviceQueryURL);
+	event.preventDefault();
+	var random = Math.floor(Math.random() * 30) + 1;
+	var queryURL =
+		"https://api.giphy.com/v1/stickers/search?q=aliens&api_key=yj2GYkgWF6vExu23oEOyov2Ydg9xDUGU&limit=1&offset=" +
+		random +
+		"&rating=g";
+	console.log(queryURL);
 
- 
+	$.ajax({
+		url: queryURL,
+		method: "GET",
+	}).then(function (response) {
+		var results = response.data;
+		console.log(results);
+
+		var imageRes = results[0].images.original.url;
+		console.log(imageRes);
+
+		// var res = $("<img>")
+		// res.attr("src", imageRes)
+		// res.attr("height", "150")
+		// res.attr("width", "150")
+		// $("#steve-image").append(res)
+
+		var steveImage = $("#steve-image");
+		steveImage.css("background-image", "url(" + imageRes + ")");
+		steveImage.css("background-repeat", "no-repeat");
+		steveImage.css("background-position", "center");
+		steveImage.css("background-size", "contain");
+	});
+});
+$("#submit").on("click", function (event) {
+	event.preventDefault();
+	var topics = $("#topics").val();
+	var adviceQueryURL = "https://api.adviceslip.com/advice/search/" + topics;
+	console.log(topics);
+	console.log(adviceQueryURL);
 
 	$.ajax({
 		url: adviceQueryURL,
 		method: "GET",
 	}).then(function (data) {
-    var obj = JSON.parse(data);
-    console.log(obj);
-    $("#advice-output").text(obj.slips[1].advice);
-    console.log(obj.slips[1].advice)
- 
+		var obj = JSON.parse(data);
+		console.log(obj);
+		var randomize = Math.floor(Math.random() * obj.slips.length);
+		$("#advice-output").text(obj.slips[randomize].advice);
+		console.log(obj.slips[randomize].advice);
 	});
 });
