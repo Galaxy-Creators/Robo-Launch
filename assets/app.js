e9929e967270bbdd53d160f7756dc436aed64ef3
Eric Kinsley <dt1993@gmail.com>
2020-04-15 21:11:53 -0500

    update firebase

--- assets/app.js
+++ assets/app.js
@@ -45,3 +45,54 @@ $("#submit").on("click", function (event) {
  
 	});
 });
+
+
+var config = {
+  apiKey: "AIzaSyCrN5q0jT45Px5plI5mMoRlTyeTFR2Aoow",
+  authDomain: "clickcountdown-a6a78.firebaseapp.com",
+  databaseURL: "https://clickcountdown-a6a78.firebaseio.com",
+  projectId: "clickcountdown-a6a78",
+  storageBucket: "clickcountdown-a6a78.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+var dataRef = firebase.database();
+
+var friendship = "";
+var food = "";
+var life = "";
+var love = "";
+var other = "";
+
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+
+  friendship = $("#frienship").val().trim();
+  food = $("food").val().trim();
+  life = $("#life").val().trim();
+  love = $("#love").val().trim();
+  other = $("#other").val().trim();
+  
+  dataRef.ref().push({
+
+    friendship: friendship,
+    food: food,
+    life: life,
+    love: love,
+    other: other,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+});
+});
+
+dataRef.ref().on("child_added", function(childSnapshot) {
+
+console.log(childSnapshot.val().name);
+      console.log(childSnapshot.val().friendship);
+      console.log(childSnapshot.val().food);
+      console.log(childSnapshot.val().life);
+      console.log(childSnapshot.val().love);
+      console.log(childSnapshot.val().other);
+
+    }); 
